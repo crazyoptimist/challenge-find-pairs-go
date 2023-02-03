882cf6d5a33be58456439576c1606a9ae9270f5a
crazyoptimist <hey@crazyoptimist.net>
2023-02-03 16:51:43 -0600

    refactor: adjust test strategy

--- findpairs_test.go
+++ findpairs_test.go
@@ -1,18 +1,32 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
 func TestFindPairs(t *testing.T) {
-	sampleSlice := []int{1, 9, 5, 0, 20, -4, 12, 16, 7}
-	sampleSum := 12
+	t.Run("it should find all the existing pairs", func(t *testing.T) {
+		nums := []int{1, 9, 5, 0, 20, -4, 12, 16, 7}
+		target := 12
 
-	got := FindPairs(sampleSlice, sampleSum)
-	want := [][]int{{12, 0}, {5, 7}, {16, -4}}
+		want := 3
+		got := len(FindPairs(nums, target))
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("expected %v, got %v", want, got)
-	}
+		if got != want {
+			t.Errorf("expected %d pairs, but got %d pairs", want, got)
+		}
+	})
+
+	t.Run("pairs should sum up to the given target", func(t *testing.T) {
+		nums := []int{1, 9, 5, 0, 20, -4, 12, 16, 7}
+		target := 12
+
+		pairs := FindPairs(nums, target)
+
+		for _, pair := range pairs {
+			if pair[0]+pair[1] != target {
+				t.Error("every pair should sum up to the target")
+			}
+		}
+	})
 }
