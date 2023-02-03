dec6b768315f4616e3519777b5ced680830d9d0b
crazyoptimist <hey@crazyoptimist.net>
2023-02-03 17:07:41 -0600

    feat: implement main func for cli

--- findpairs.go
+++ findpairs.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func FindPairs(nums []int, target int) (result [][]int) {
 	hashmap := make(map[int]int)
 	for idx, num := range nums {
@@ -10,4 +17,22 @@ func FindPairs(nums []int, target int) (result [][]int) {
 		hashmap[num] = idx
 	}
 	return
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: <app> <integers comma separated> <target integer>")
+		return
+	}
+
+	nums := make([]int, 0)
+	target, _ := strconv.Atoi(os.Args[2])
+
+	for _, numberString := range strings.Split(os.Args[1], ",") {
+		num, _ := strconv.Atoi(numberString)
+		nums = append(nums, num)
+	}
+
+	fmt.Println(FindPairs(nums, target))
+
 }
