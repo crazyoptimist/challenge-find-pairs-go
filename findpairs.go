d8baff633a3e7980aebdf9dae536b3574d3f41b3
crazyoptimist <hey@crazyoptimist.net>
2023-02-03 16:45:42 -0600

    feat: write working code

--- findpairs.go
+++ findpairs.go
@@ -1,5 +1,13 @@
 package main
 
-func FindPairs(inputSlice []int, inputSum int) [][]int {
-	return nil
+func FindPairs(nums []int, target int) (result [][]int) {
+	hashmap := make(map[int]int)
+	for idx, num := range nums {
+		if _, found := hashmap[target-num]; found {
+			result = append(result, []int{num, target - num})
+		}
+
+		hashmap[num] = idx
+	}
+	return
 }
